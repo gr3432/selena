4b1618e4c7e574ec618a1c34f87ca564dcf98b0c
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-19 11:33:00 +0200

    Fix page encoding before printing

--- test_foo.py
+++ test_foo.py
@@ -42,6 +42,7 @@ def test_foo(driver):
     settings_link.click()
     time.sleep(5)
     page = driver.page_source
+    page.encode('utf-8')
     print(page)
     cookies_pref_locator = "//a[@href='/settings/cookie_preferences']"
     condition = lambda driver: driver.find_element(By.XPATH, cookies_pref_locator)

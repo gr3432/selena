d3c1d27d448bafa97965e8ae838edf6e375a0e7b
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-19 11:39:15 +0200

    Fix encoding on print

--- test_foo.py
+++ test_foo.py
@@ -42,8 +42,7 @@ def test_foo(driver):
     settings_link.click()
     time.sleep(5)
     page = driver.page_source
-    page.encode('utf-8')
-    print(page)
+    print(page.encode('utf-8'))
     cookies_pref_locator = "//a[@href='/settings/cookie_preferences']"
     condition = lambda driver: driver.find_element(By.XPATH, cookies_pref_locator)
     cookies_pref = WebDriverWait(driver, timeout=timeout).until(condition)

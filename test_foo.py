dd3119ec3fd2111279d3d719cff0a71971b818dd
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-19 10:45:35 +0200

    Increase timeout for finding elements

--- test_foo.py
+++ test_foo.py
@@ -8,6 +8,8 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+timeout = 10
+
 @pytest.fixture(scope="module")
 def chrome_driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -29,10 +31,10 @@ def test_foo(driver):
     driver.get("https://twitter.com")
     settings_locator = "//header//*//a[@aria-label='Settings']"
     condition = lambda driver: driver.find_element(By.XPATH, settings_locator)
-    settings_link = WebDriverWait(driver, timeout=5).until(condition)
+    settings_link = WebDriverWait(driver, timeout=timeout).until(condition)
     settings_link.click()
     cookies_pref_locator = "//a[@href='/settings/cookie_preferences']"
     condition = lambda driver: driver.find_element(By.XPATH, cookies_pref_locator)
-    cookies_pref = WebDriverWait(driver, timeout=5).until(condition)
+    cookies_pref = WebDriverWait(driver, timeout=timeout).until(condition)
     assert "Cookie" in cookies_pref.text
 

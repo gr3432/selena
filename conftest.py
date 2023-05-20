c3d1bfa43b6602beda339ad9fa8f1a463833757b
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-20 23:25:34 +0200

    Use webdriver manager

--- conftest.py
+++ conftest.py
@@ -2,15 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
-from constants import webdriver_edge_path
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 @pytest.fixture(scope="session")
 def driver():  
     options = Options()
     options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    # options.add_argument("--headless")
 
-    service = Service(executable_path=webdriver_edge_path)
+    service = Service(EdgeChromiumDriverManager().install())
     driver = webdriver.Edge(service=service, options=options)
     
     return driver

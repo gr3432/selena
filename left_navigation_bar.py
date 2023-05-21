5436eed1bfa25d0045ede3d26cb17ecda83f2cb4
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-21 20:21:18 +0200

    Add page for left navigation bar

---
+++ left_navigation_bar.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Locator:
+    dashboard_button = (By.XPATH, "//app-left-nav/aside/ul/li[1]/a")
+    organization_button = (By.XPATH, "//app-left-nav/aside/ul/li[2]/a")
+    organization_submenu = (By.XPATH, "//app-left-nav/aside/ul/li[2]/ul")
+    employees_button = (By.XPATH, "//app-left-nav/aside/ul/li[2]/ul/li[2]/a")
+
+
+class LeftNavigationBar:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def navigate_to_dashboard(self):
+        dashboard_button = self.driver.find_element(*Locator.dashboard_button)
+        dashboard_button.click()
+
+    def navigate_to_employees(self):
+        organization_button = self.driver.find_element(*Locator.organization_button)
+        if not "open" in organization_button.find_element(By.XPATH, "./div[2]").get_attribute("class"):
+            organization_button.click()
+        organization_submenu = self.driver.find_element(*Locator.organization_submenu)
+        if "opacity: 0" in organization_submenu.get_attribute("style"):
+            organization_button.click()
+        employees_button = self.driver.find_element(*Locator.employees_button)
+        employees_button.click()

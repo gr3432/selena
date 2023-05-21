f3838362ed827d5f1d6e719351b34c18a12f9eba
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-21 22:25:34 +0200

    Add base page class

---
+++ base_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+TIMEOUT = 10
+
+class Locators:
+    top_bar_header = (By.XPATH, "//app-top-bar/mat-toolbar/div[1]/span")
+
+class Basepage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def page_loaded(self):
+        try:
+            WebDriverWait(self.driver, TIMEOUT).until(
+                lambda driver: driver.find_element(*Locators.top_bar_header)
+            )
+            return True
+        except TimeoutException:
+            return False
+    

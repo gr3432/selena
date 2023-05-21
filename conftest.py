52cbd30b67bfa329d07a541370c32a1af45668d7
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-21 22:27:51 +0200

    Add driver_with_cookies session fixture

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
+import pickle
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from constants import login_page_url, landing_page_url
 
 
 @pytest.fixture(scope="session")
@@ -15,3 +17,23 @@ def driver():
     driver = webdriver.Edge(service=service, options=options)
     
     return driver
+
+@pytest.fixture(scope="session")
+def driver_with_cookies():
+    # cookies.pkl was created with pickle.dump
+    with open("cookies.pkl", "rb") as f:
+        cookies = pickle.load(f)
+
+    service = Service(EdgeChromiumDriverManager().install())
+    driver = webdriver.Edge(service=service)
+
+    driver.get(login_page_url)
+    
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    
+    driver.get(landing_page_url)
+    
+    yield driver
+
+    driver.quit()

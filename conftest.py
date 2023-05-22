b001830e30efceba894eac600446c9e03295d2c2
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-22 21:30:03 +0200

    Add --environment-secrets pytest option

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from constants import login_page_url, landing_page_url, test_email, test_password
 from login_page import Login
 from base_page import Basepage
 
@@ -15,6 +14,23 @@ def pytest_addoption(parser):
         action = "store_true",
         help = "Run driver in headless mode"
     )
+    parser.addoption(
+        "--environment-secrets",
+        action = "store_true",
+        help = "Look for secrets in environment"
+    )
+
+def pytest_configure(config):
+    if config.getoption("--environment-secrets"):
+        import os
+        pytest.test_email = os.environ['test_email']
+        pytest.test_password = os.environ['test_password']
+        pytest.login_page_url = os.environ['login_page_url']
+    else:
+        from constants import test_email, test_password, login_page_url
+        pytest.test_email = test_email
+        pytest.test_password = test_password
+        pytest.login_page_url = login_page_url
 
 
 @pytest.fixture(scope="session")
@@ -37,12 +53,12 @@ def driver_with_login(request):
     driver = webdriver.Edge(service=service, options=options)
 
     driver.maximize_window()
-    driver.get(login_page_url)
+    driver.get(pytest.login_page_url)
 
     login_page = Login(driver)
     login_page.select_ID_login()
-    login_page.input_email(test_email)
-    login_page.input_password(test_password)
+    login_page.input_email(pytest.test_email)
+    login_page.input_password(pytest.test_password)
     login_page.click_sign_in()
     login_page.select_stay_signed_in()
     base_page = Basepage(login_page.driver)
@@ -65,12 +81,12 @@ def driver_with_cookies():
     service = Service(EdgeChromiumDriverManager().install())
     driver = webdriver.Edge(service=service)
 
-    driver.get(login_page_url)
+    driver.get(pytest.login_page_url)
     
     for cookie in cookies:
         driver.add_cookie(cookie)
     
-    driver.get(landing_page_url)
+    driver.get(pytest.landing_page_url)
     
     yield driver
 

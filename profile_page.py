c76a6af259d682c7d116cb84bbdb39dccdb18347
Georgios Rizothanasis <trivialgeorge@gmail.com>
2023-05-23 09:32:13 +0200

    Add print for debugging

--- profile_page.py
+++ profile_page.py
@@ -13,39 +13,18 @@ class Locator():
     first_name_input = (By.XPATH, "//app-profile-details-info-card/app-details-info-card/div/div[2]/div/app-profile-details-form/div[1]/app-text-input[1]/mat-form-field/div/div[1]/div[3]/input")
     first_name_text = (By.XPATH, "//app-profile-details-info-card/app-details-info-card/div/div[2]/div/app-profile-details-form/div[1]/app-text-input[1]/div[2]")
     save_profile_button = (By.XPATH, "//div//button[span[text()='Save']]")
-    profile_details_button = (By.XPATH, "//app-tabs-left-group/ul/li[1]/a")
     profile_details_header = (By.XPATH, "//app-profile-details/app-details-info-title/h3")
-    organization_unit_button = (By.XPATH, "//app-tabs-left-group/ul/li[2]/a")
-    organization_unit_header = (By.XPATH, "//app-consumer-care-providers/app-details-info-title/h3")
 
 
 class Profile:
     def __init__(self, driver):
         self.driver = driver
 
-    def view_profile_details(self):
-        profile_details_button = WebDriverWait(self.driver, TIMEOUT).until(
-            EC.element_to_be_clickable(Locator.profile_details_button))
-        actions = ActionChains(self.driver)
-        actions.move_to_element(profile_details_button).click().perform()
-        #profile_details_button.click()
-
     def get_profile_details_header(self):
         profile_details_header = WebDriverWait(self.driver, TIMEOUT).until(
             lambda driver: driver.find_element(*Locator.profile_details_header))
         return profile_details_header.text
 
-    def view_organization_unit(self):
-        organization_unit_button = WebDriverWait(self.driver, TIMEOUT).until(
-            EC.element_to_be_clickable(Locator.organization_unit_button))
-        actions = ActionChains(self.driver)
-        actions.move_to_element(organization_unit_button).click().perform()
-        
-    def get_organization_unit_header(self):
-        organization_unit_header = WebDriverWait(self.driver, TIMEOUT).until(
-            lambda driver: driver.find_element(*Locator.organization_unit_header))
-        return organization_unit_header.text
-    
     def edit_profile(self):
         edit_profile_button = WebDriverWait(self.driver, TIMEOUT).until(
             EC.element_to_be_clickable(Locator.edit_profile_button))
@@ -58,6 +37,11 @@ class Profile:
         return first_name_text.text
 
     def edit_first_name(self, name):
+        parent_loc = (By.XPATH, "//app-profile-details-info-card/app-details-info-card/div/div[2]/div/app-profile-details-form/div[1]/app-text-input[1]")
+        parent = WebDriverWait(self.driver, TIMEOUT).until(
+            lambda driver: driver.find_element(*parent_loc))
+        print(parent.get_attribute("innerHTML"))
+
         input_field = WebDriverWait(self.driver, TIMEOUT).until(
             lambda driver: driver.find_element(*Locator.first_name_input))
         input_field.clear()
